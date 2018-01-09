5a518b8bbfe4b66b4e8d0bf95de6f012b033eccc
hcpizzi <hcpizzi@hotmail.com>
2018-01-09 19:47:06 +0100

    Missing source assets

---
+++ Content/App.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ShooterGame
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //public App()
+        //{
+        //    Activated += OnActivated;
+        //}
+
+        //private void OnActivated(object sender, EventArgs e)
+        //{
+        //    var data = new Expression.Blend.SampleData.GameMenuSampleData.GameMenuSampleData();
+        //    data.Resolutions.Clear();
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "800x600" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "1280x720" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "1280x768" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "1600x900" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "1920x1080" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "1920x1200" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "800x600" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "1280x720" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "1280x768" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "1600x900" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "1920x1080" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "1920x1200" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "800x600" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "1280x720" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "1280x768" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "1600x900" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "1920x1080" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "1920x1200" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "800x600" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "1280x720" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "1280x768" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "1600x900" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "1920x1080" });
+        //    data.Resolutions.Add(new Expression.Blend.SampleData.GameMenuSampleData.ResolutionsItem { Value = "1920x1200" });
+        //    this.MainWindow.DataContext = data;
+        //}
+    }
+}
